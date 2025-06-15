6547ea93e47437e9936d983577ac4f858d205500
ginigo-arg <gabrielinigo99@gmail.com>
2025-06-16 01:01:25 +0200

    feat: filter available products

--- app/products/actions/get-products.ts
+++ app/products/actions/get-products.ts
@@ -4,5 +4,11 @@ import { get } from "@/app/common/util/fetch";
 import { Product } from "../interfaces/product-interface";
 
 export async function getProducts() {
-  return get<Product[]>("products", ["products"]);
+  return get<Product[]>(
+    "products",
+    ["products"],
+    new URLSearchParams({
+      status: "available",
+    })
+  );
 }
